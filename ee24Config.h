7b429f30974d84e31f329f466846b7dffc0e9535
ruslikc <ruslikc@gmail.com>
2021-05-14 14:38:33 +0300

    adjust config from the outside project

--- ee24Config.h
+++ ee24Config.h
@@ -1,11 +1,11 @@
 #ifndef	_EE24CONFIG_H
 #define	_EE24CONFIG_H
 
-#define		_EEPROM_SIZE_KBIT							16
-#define		_EEPROM_I2C   								hi2c3
-#define		_EEPROM_USE_FREERTOS          0
-#define		_EEPROM_ADDRESS               0xA0
-#define		_EEPROM_USE_WP_PIN            0
+#define		_EEPROM_SIZE_KBIT							EEPROM_SIZE_KBIT //define this in another user file.
+#define		_EEPROM_I2C   								EEPROM_I2C
+#define		_EEPROM_USE_FREERTOS          EEPROM_USE_FREERTOS
+#define		_EEPROM_ADDRESS               EEPROM_ADDRESS
+#define		_EEPROM_USE_WP_PIN            EEPROM_USE_WP_PIN
 
 #if (_EEPROM_USE_WP_PIN==1)
 #define		_EEPROM_WP_GPIO								EE_WP_GPIO_Port
